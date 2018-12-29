afba4302becebb44eebea17d9c66bc58566c0a5a
Mateusz Górski <mgorski.lbn@gmail.com>
2018-12-29 16:30:11 +0100

    Finished table

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 (function () {
     'use strict';
 
-    const employees = [{
-            firstName: 'John',
-            lastName: 'Doe',
-            age: 31,
-            sex: 'male',
-            employed: true
-        },
-        {
-            firstName: 'Amelia',
-            lastName: 'Clark',
-            age: 22,
-            sex: 'female',
-            employed: true
-        },
-        {
-            firstName: 'Jack',
-            lastName: 'Jones',
-            age: 21,
-            sex: 'male',
-            employed: false
-        },
-        {
-            firstName: 'Olivia',
-            lastName: 'Wilde',
-            age: 31,
-            sex: 'female',
-            employed: true
-        },
-    ]
+    function getEmpolyees() {
+        const savedEmployees = localStorage.getItem("Employees");
+        if (savedEmployees) {
+            return JSON.parse(savedEmployees);
+        } else {
+            const employees = [{
+
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    age: 31,
+                    sex: 'male',
+                    employed: true
+                },
+                {
+                    firstName: 'Amelia',
+                    lastName: 'Clark',
+                    age: 22,
+                    sex: 'female',
+                    employed: true
+                },
+                {
+                    firstName: 'Jack',
+                    lastName: 'Jones',
+                    age: 21,
+                    sex: 'male',
+                    employed: false
+                },
+                {
+                    firstName: 'Olivia',
+                    lastName: 'Wilde',
+                    age: 31,
+                    sex: 'female',
+                    employed: true
+                },
+            ]
+            localStorage.setItem("Employees", JSON.stringify(employees));
+
+            return JSON.parse(localStorage.getItem("Employees"));
+        }
+
+    }
 
     function renderHeaders() {
         const tableHeaders = Object.keys(employees[0]);
@@ -40,7 +52,6 @@
         });
         tableHeadersDOM.innerHTML = htmlHeaders;
     }
-
 
     function renderTableBody(employeesArray) {
         const tableValuesDOM = document.getElementById("table-values");
@@ -77,15 +88,44 @@
         renderTableBody(employees);
     }
 
+    function addNewEmployee() {
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const employeeAge = document.getElementById("age").value;
+        const employeeSex = document.getElementById("sexSelect").value;
+        const employedStatus = document.getElementById("employed").value;
+
+        if (firstName && lastName && employeeAge && employeeSex) {
+            const newEmployee = {
+                firstName: firstName,
+                lastName: lastName,
+                age: employeeAge,
+                sex: employeeSex,
+                employed: employedStatus,
+            }
+            employees.push(newEmployee);
+            localStorage.setItem("Employees", JSON.stringify(employees));
+            renderTableBody(employees);
+        } else {
+            alert("Please fill all the fields");
+        }
+    }
+
+    const employees = getEmpolyees()
     renderHeaders();
     renderTableBody(employees);
 
+
     const filterWomanButton = document.getElementById("woman-button");
-    filterWomanButton.addEventListener("click", filterWoman);
+    filterWomanButton.addEventListener("click", filterMan);
 
     const filterManButton = document.getElementById("man-button");
-    filterManButton.addEventListener("click", filterMan);
+    filterManButton.addEventListener("click", filterWoman);
 
     const refreshButton = document.getElementById("refresh");
     refreshButton.addEventListener("click", refreshTable);
+
+    const addNewEmployeeButton = document.getElementById("new-employee-button");
+    addNewEmployeeButton.addEventListener("click", addNewEmployee);
+
 }());
